9bce8df6014cb7096eda8141e0c4400e43826061
Gabriel Sousa <gabrielsousa140697@yahoo.com>
2020-10-20 13:30:24 -0300

    Angular 2 Modules

--- primeiro-projeto/src/app/app.module.ts
+++ primeiro-projeto/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+//MODULO RAIZ
 
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';  //Prepara a aplicação para ser exec em um browser
+import { NgModule } from '@angular/core'; //Decorator @NgModule: seta o que a classe vai ser e conter
+
+import { AppRoutingModule } from './app-routing.module'; // módulo de formulário
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 
-@NgModule({
-  declarations: [
+
+@NgModule({       //Decorator @NgModule: seta o que a classe vai ser e conter 
+  declarations: [ //Meta dados do modulo
     AppComponent
   ],
-  imports: [
+  imports: [ //Meta dados do modulo
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
